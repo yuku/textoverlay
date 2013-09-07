06d039eaca8f6172ef2d0f1192907ae2f901be7d
Yuku Takahashi <taka84u9@gmail.com>
2013-09-08 01:18:35 +0900

    Start implementing jquery.textoverlay.js

---
+++ jquery.textoverlay.js
@@ -0,0 +1,13 @@
+/*!
+ * jQuery.textoverlay.js
+ *
+ * Repository: https://github.com/yuku-t/jquery-textoverlay
+ * License:    MIT
+ * Author:     Yuku Takahashi
+ */
+
+;(function ($) {
+
+  'use strict';
+
+})(window.jQuery);
